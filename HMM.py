748dfb3cf8843c0acde5e97ea53ca02e6802b943
Kyota Lannelongue <kyota95@live.fr>
2017-10-27 00:01:27 +0200

    Updates working
    but alpha is weird

--- HMM.py
+++ HMM.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # init variables
 a = np.matrix([[1, 0, 0, 0], [0.2, 0.3, 0.1, 0.4],
@@ -28,9 +29,11 @@ def forward(a, b, x):
             else:
                 sum = 0
                 for i in range(N):
-                    sum += a[i, j] * alpha[i, t-1]
-                alpha[j, t] = b[j, x[t - 1]] * sum
+                    alpha[j, t] += a[i, j] * alpha[i, t-1]
+                print(alpha[i, t-1])
+                alpha[j, t] *= b[j, x[t - 1]]
                 #alpha[j, t] = b[j, x[t-1]]*np.sum(np.multiply(a[:, j], alpha[:, t-1]))
+    print("alpha =")
     print(alpha)
 
     probmodel = alpha[0, T - 1]
@@ -41,20 +44,23 @@ def forward(a, b, x):
 def backward(a, b, x):
     print("Backward")
     beta = np.zeros((N, T))
+    beta[0, T - 1] = 1
 
-    for t in reversed(range(T)):
+    for t in range(T-2, -1, -1):
         for i in range(N):
-            if t == T - 1:
-                if i == 0:
-                    beta[i, t] = 1
-                else:
-                    beta[i, t] = 0
-            else:
-                for j in range(N):
-                    beta[i, t] += beta[j, t + 1] * a[i, j] * b[j, x[t]]
-    print(beta)
+        #    if t == T - 1:
+
+         #       if i == 0:
+          #          beta[i, t] = 1
+           #     else:
+            #        beta[i, t] = 0
+        #    else:
+
+            for j in range(N):
+                beta[i, t] += beta[j, t + 1] * a[i, j] * b[j, x[t]]
 
     probmodel = beta[N - 1, 0]
+    print(beta)
     print("Probamodel backward " + str(probmodel))
     return beta
 
@@ -62,21 +68,13 @@ def estimate_gamma(alpha, beta, a, b, x):
     print("Estimate gamma")
     gamma = np.zeros((N, N, T))
     for t in range(1, T):
-
         for i in range(N):
-
             for j in range(N):
-#                print("-----------")
-#                print("t=" + str(t))
- #               print("i=" + str(i))
-  #              print("j=" + str(j))
-   #             print("alpha" + str(alpha[i, t - 1]))
-    #            print("a" + str(a[i, j]))
-     #           print("b" + str(b[j, x[t-1]]))
-      #          print("beta" + str(beta[j, t]))
-                gamma[i, j, t] = alpha[i, t - 1] * a[i, j] * b[j, x[t-1]] * beta[j, t]
+                gamma[i, j, t] = alpha[i, t - 1] * a[i, j] * b[j, x[t - 1]] * beta[j, t]
 
-               # print(gamma[i, j, t])
+
+   # print("gamma=")
+    #print(gamma)
     return gamma
 
 def update_a(a, gamma, x):
@@ -99,45 +97,47 @@ def update_a(a, gamma, x):
             #print("den" + str(den))
 
             a[i, j] = nom / den
-            a[0, 0] = 1
+    a[0, 0] = 1
     return a
 
 def update_b(b, gamma, x):
     print("Update b")
+
     for j in range(N):
-        for k in range(N):
+        for k in range(T-1):
             den = 0
             nom = 0
+
             for t in range(1, T):
-                print("t=" + str(t))
-                for l in range(N):
-                    if x[t - 1] == k:
-                        print(gamma[j, l, t])
-                        nom += gamma[j, l, t]
-                    den += gamma[j, l, t]
-
-            if den == 0:
-                den += 1
-
-            b[j, k] = nom / den
-
+                if x[t - 1] == k:
+                    b[j, k + 1] += np.sum(gamma[j, :, t])
+                    #if t == 3 and k == 2:
+                        #print("allo")
+                        #print(np.sum(gamma[j, :, t]))
+            #print(b[j, k + 1])
+            for t in range(1, T):
+                den += np.sum(gamma[j, :, t])
+                if den == 0:
+                    den += 1
+            b[j, k + 1] = b[j, k + 1] / den
     b[0, 0] = 1
     return b
 
 def baum_welch(x):
-    print("Baum Welsh")
+    print("Baum Welch")
     a = np.matrix([[1, 0, 0, 0], [0.2, 0.3, 0.1, 0.4], [0.2, 0.5, 0.2, 0.1],
                    [0.7, 0.1, 0.1, 0.1]])  # Transition proba (i=nb of state j=nb of sample)
     b = np.matrix([[1, 0, 0, 0, 0], [0, 0.3, 0.4, 0.1, 0.2], [0, 0.1, 0.1, 0.7, 0.1],
                    [0, 0.5, 0.2, 0.1, 0.2]])  # Emission proba -j=nb of state k=nb of output)
 
-    alpha = np.zeros((N, T))
-    beta = np.zeros((N, T))
-    gamma = np.zeros((N, N, T))
+    print(a)
+    print(b)
     iter = 0
 
-    while iter in range(2):
+    while iter in range(10):
         print("Iteration: " + str(iter))
+
+
         # Estimate alpha and beta
         alpha = forward(a, b, x)
         beta = backward(a, b, x)
